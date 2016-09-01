aa9da968332dc2e3c6473bb6980620352119f44d
teramotodaiki <i101323@gmail.com>
2016-09-01 10:34:58 +0900

    render all

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 const Hogan = require('hogan.js');
-const copyright = require('../templates/').copyright;
 
-console.log(copyright.render({ year: 2016 }));
-console.log('Hey! I am hackforplayer!');
+const content = require('../templates/').content;
+
+const classNamePrefix = '.h4p__';
+const src = 'http://localhost:3000/index.html';
+
+window.onload = () => {
+
+  Array.prototype.slice.call(document.querySelectorAll(classNamePrefix + 'content'))
+    .map((element) => {
+      const props = { src };
+      element.innerHTML = content.render(props);
+    });
+
+};

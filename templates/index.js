7f978868c739b15df916f67da494011888167a80
teramotodaiki <i101323@gmail.com>
2016-09-01 10:34:23 +0900

    add template

--- templates/index.js
+++ templates/index.js
@@ -1,5 +1,6 @@
 const Hogan = require('hogan.js');
 
 module.exports = {
+  content: new Hogan.Template(require('../hogan-loader!./content.html')),
   copyright: new Hogan.Template(require('../hogan-loader!./copyright.html'))
 };

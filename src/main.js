4753f75f7ff01d15fd0155874767e9e8da71ff9b
teramotodaiki <i101323@gmail.com>
2016-09-05 11:42:32 +0900

    Fix position and size on resized

--- src/main.js
+++ src/main.js
@@ -22,6 +22,23 @@ const init = () => {
         player.start([], query && document.querySelector(query).textContent);
       });
 
+      // Always contains in screen and stay bottom
+      player.addEventListener('resize', (event) => {
+        const screenRect = event.screenRect;
+        const contentSize = event.contentSize;
+
+        const ratio = (size) => Math.max(size.height, 1) / Math.max(size.width, 1);
+        if (ratio(screenRect) > ratio(contentSize)) {
+          iframe.width = screenRect.width;
+          iframe.height = screenRect.width * ratio(contentSize);
+          iframe.style.left = screenRect.left + 'px';
+          iframe.style.top = screenRect.top + (screenRect.height - iframe.height) + 'px';
+        } else {
+          iframe.width = screenRect.height / ratio(contentSize);
+          iframe.height = screenRect.height;
+          iframe.style.left = screenRect.left + (screenRect.width - iframe.width) / 2 + 'px';
+          iframe.style.top = screenRect.top + 'px';
+        }
       });
 
       return player;

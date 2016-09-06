f4b8093a5d5c94e5a55231b0595e82adaa9bfd21
teramotodaiki <i101323@gmail.com>
2016-09-06 21:26:01 +0900

    Independent dock from template

--- templates/index.js
+++ templates/index.js
@@ -3,6 +3,7 @@ const Hogan = require('hogan.js');
 module.exports = {
   content: new Hogan.Template(require('../hogan-loader!./content.html')),
   button: new Hogan.Template(require('../hogan-loader!./button.html')),
+  dock: new Hogan.Template(require('../hogan-loader!./dock.html')),
   editor: new Hogan.Template(require('../hogan-loader!./editor.html')),
   copyright: new Hogan.Template(require('../hogan-loader!./copyright.html'))
 };

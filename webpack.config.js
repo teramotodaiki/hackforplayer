092ea77dcdd4d141e22eeccff903d5122ce42ea3
teramotodaiki <i101323@gmail.com>
2016-09-06 18:16:51 +0900

    webpack css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,13 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'hackforplayer.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
   }
 };

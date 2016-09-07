d3a9b7d30a070075edf40c818712b77fc7effa6c
teramotodaiki <i101323@gmail.com>
2016-09-07 15:28:06 +0900

    alignment task

--- src/stayBottom.js
+++ src/stayBottom.js
@@ -1,19 +1,22 @@
-module.exports =
-(iframe) =>
-  (event) => {
-    const screenRect = event.screenRect;
-    const contentSize = event.contentSize;
+const getElementRect = require('./getElementRect');
 
-    const ratio = (size) => Math.max(size.height, 1) / Math.max(size.width, 1);
-    if (ratio(screenRect) > ratio(contentSize)) {
-      iframe.width = screenRect.width;
-      iframe.height = screenRect.width * ratio(contentSize);
-      iframe.style.left = screenRect.left + 'px';
-      iframe.style.top = screenRect.top + (screenRect.height - iframe.height) + 'px';
-    } else {
-      iframe.width = screenRect.height / ratio(contentSize);
-      iframe.height = screenRect.height;
-      iframe.style.left = screenRect.left + (screenRect.width - iframe.width) / 2 + 'px';
-      iframe.style.top = screenRect.top + 'px';
-    }
-  };
+module.exports = ({dom, player, iframe}) =>
+() => {
+  if (!dom.refs.screen) return;
+  const screenRect = getElementRect(dom.refs.screen);
+  const iframeRect = getElementRect(iframe);
+  const contentSize = player.getContentSize();
+
+  const ratio = (size) => Math.max(size.height, 1) / Math.max(size.width, 1);
+  if (ratio(screenRect) > ratio(contentSize)) {
+    iframe.width = screenRect.width;
+    iframe.height = screenRect.width * ratio(contentSize);
+    iframe.style.left = screenRect.left + 'px';
+    iframe.style.top = screenRect.top + (screenRect.height - iframe.height) + 'px';
+  } else {
+    iframe.width = screenRect.height / ratio(contentSize);
+    iframe.height = screenRect.height;
+    iframe.style.left = screenRect.left + (screenRect.width - iframe.width) / 2 + 'px';
+    iframe.style.top = screenRect.top + 'px';
+  }
+};

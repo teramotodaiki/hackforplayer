a56404054a0c2980f5a25e251b552d92fc464ec5
teramotodaiki <i101323@gmail.com>
2016-09-07 09:40:16 +0900

    remove old

--- templates/index.js
+++ templates/index.js
@@ -4,6 +4,5 @@ module.exports = {
   content: new Hogan.Template(require('../hogan-loader!./content.html')),
   button: new Hogan.Template(require('../hogan-loader!./button.html')),
   dock: new Hogan.Template(require('../hogan-loader!./dock.html')),
-  editor: new Hogan.Template(require('../hogan-loader!./editor.html')),
-  copyright: new Hogan.Template(require('../hogan-loader!./copyright.html'))
+  editor: new Hogan.Template(require('../hogan-loader!./editor.html'))
 };

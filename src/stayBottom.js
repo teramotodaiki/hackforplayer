b54cfcd43c97bf45db158005a82b6c5aa3e6d470
teramotodaiki <i101323@gmail.com>
2016-09-08 18:39:55 +0900

    default is prevent

--- src/stayBottom.js
+++ src/stayBottom.js
@@ -1,28 +1,41 @@
-module.exports = ({dom, player, iframe}) =>
-() => {
-  if (!dom.refs.screen) return;
-  const screenRect = dom.refs.screen.getBoundingClientRect();
-  const iframeStyle = getComputedStyle(iframe);
-  const offset = {
-    x: iframeStyle.position === 'absolute' ? pageXOffset : 0,
-    y: iframeStyle.position === 'absolute' ? pageYOffset : 0,
+module.exports = (dom) => {
+
+  var _frame, _width, _height;
+
+  return ({frame, width, height}) => {
+    _frame = typeof frame === 'undefined' ? _frame : frame;
+    _width = typeof width === 'undefined' ? _width : width;
+    _height = typeof height === 'undefined' ? _height : height;
+
+    resizeHandler(dom, _frame, _width, _height);
   };
-  const contentSize = player.getContentSize();
+
+};
+
+function resizeHandler(dom, frame, width, height) {
+  if (!dom.refs.screen || !frame) return;
+  const screenRect = dom.refs.screen.getBoundingClientRect();
+  const frameStyle = getComputedStyle(frame);
+  const offset = {
+    x: frameStyle.position === 'absolute' ? pageXOffset : 0,
+    y: frameStyle.position === 'absolute' ? pageYOffset : 0,
+  };
+  const contentSize = { width, height };
 
   const ratio = (size) => Math.max(size.height, 1) / Math.max(size.width, 1);
   if (ratio(screenRect) > ratio(contentSize)) {
-    iframe.width = screenRect.width;
-    iframe.height = screenRect.width * ratio(contentSize);
-    iframe.style.width = screenRect.width + 'px';
-    iframe.style.height = screenRect.width * ratio(contentSize) + 'px';
-    iframe.style.left = screenRect.left + offset.x + 'px';
-    iframe.style.top = screenRect.top + (screenRect.height - iframe.height) + offset.y + 'px';
+    frame.width = screenRect.width;
+    frame.height = screenRect.width * ratio(contentSize);
+    frame.style.width = screenRect.width + 'px';
+    frame.style.height = screenRect.width * ratio(contentSize) + 'px';
+    frame.style.left = screenRect.left + offset.x + 'px';
+    frame.style.top = screenRect.top + (screenRect.height - frame.height) + offset.y + 'px';
   } else {
-    iframe.width = screenRect.height / ratio(contentSize);
-    iframe.height = screenRect.height;
-    iframe.style.width = screenRect.height / ratio(contentSize) + 'px';
-    iframe.style.height = screenRect.height + 'px';
-    iframe.style.left = screenRect.left + (screenRect.width - iframe.width) / 2 + offset.x + 'px';
-    iframe.style.top = screenRect.top + offset.y + 'px';
+    frame.width = screenRect.height / ratio(contentSize);
+    frame.height = screenRect.height;
+    frame.style.width = screenRect.height / ratio(contentSize) + 'px';
+    frame.style.height = screenRect.height + 'px';
+    frame.style.left = screenRect.left + (screenRect.width - frame.width) / 2 + offset.x + 'px';
+    frame.style.top = screenRect.top + offset.y + 'px';
   }
 };

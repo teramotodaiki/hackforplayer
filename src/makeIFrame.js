b2772ed4a9bb4f9d4fd4212479ca04c2e001cd00
teramotodaiki <i101323@gmail.com>
2016-09-08 12:49:16 +0900

    floating object can set css position absolute or fixed

--- src/makeIFrame.js
+++ src/makeIFrame.js
@@ -2,6 +2,7 @@ const initPosition = require('./initPosition');
 
 module.exports = () => {
   const iframe = document.createElement('iframe');
+  iframe.style.position = 'absolute';
   initPosition(iframe, 'top left');
   document.body.appendChild(iframe);
   return iframe;
--- src/stayBottom.js
+++ src/stayBottom.js
@@ -1,22 +1,28 @@
-const getElementRect = require('./getElementRect');
-
 module.exports = ({dom, player, iframe}) =>
 () => {
   if (!dom.refs.screen) return;
-  const screenRect = getElementRect(dom.refs.screen);
-  const iframeRect = getElementRect(iframe);
+  const screenRect = dom.refs.screen.getBoundingClientRect();
+  const iframeStyle = getComputedStyle(iframe);
+  const offset = {
+    x: iframeStyle.position === 'absolute' ? pageXOffset : 0,
+    y: iframeStyle.position === 'absolute' ? pageYOffset : 0,
+  };
   const contentSize = player.getContentSize();
 
   const ratio = (size) => Math.max(size.height, 1) / Math.max(size.width, 1);
   if (ratio(screenRect) > ratio(contentSize)) {
     iframe.width = screenRect.width;
     iframe.height = screenRect.width * ratio(contentSize);
-    iframe.style.left = screenRect.left + 'px';
-    iframe.style.top = screenRect.top + (screenRect.height - iframe.height) + 'px';
+    iframe.style.width = screenRect.width + 'px';
+    iframe.style.height = screenRect.width * ratio(contentSize) + 'px';
+    iframe.style.left = screenRect.left + offset.x + 'px';
+    iframe.style.top = screenRect.top + (screenRect.height - iframe.height) + offset.y + 'px';
   } else {
     iframe.width = screenRect.height / ratio(contentSize);
     iframe.height = screenRect.height;
-    iframe.style.left = screenRect.left + (screenRect.width - iframe.width) / 2 + 'px';
-    iframe.style.top = screenRect.top + 'px';
+    iframe.style.width = screenRect.height / ratio(contentSize) + 'px';
+    iframe.style.height = screenRect.height + 'px';
+    iframe.style.left = screenRect.left + (screenRect.width - iframe.width) / 2 + offset.x + 'px';
+    iframe.style.top = screenRect.top + offset.y + 'px';
   }
 };

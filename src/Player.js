079c5f2707a82b533bd336066b48a26b673a12f1
teramotodaiki <i101323@gmail.com>
2016-09-13 12:47:23 +0900

    remove old

--- src/Player.js
+++ src/Player.js
@@ -11,19 +11,6 @@ class Player extends EventTarget {
 
     this.src = src;
 
-    var width = 300, height = 150;
-    this.getContentSize = () => ({ width, height });
-    this.addEventListener('resize', () => {
-      width = event.data.width;
-      height = event.data.height;
-    });
-
-    // cannot access port directly
-    var port = null;
-    this.setPort = (value) => {
-      port = this._setPort(value, port);
-    };
-
     this.addEventListener('beforeunload', (event) => event.child.destroy());
   }
 
@@ -70,50 +57,6 @@ class Player extends EventTarget {
     this.dispatchEvent(event);
   }
 
-  standBy(contentWindow) {
-    return new Promise((resolve, reject) => {
-      addEventListener('message', function task(event) {
-        if (event.source !== contentWindow) return;
-        removeEventListener('message', task);
-        resolve(event);
-      });
-    });
-  }
-
-  connect(contentWindow) {
-    return new Promise((resolve, reject) => {
-
-      // TODO: Request to reload if connected
-
-      this.standBy(contentWindow)
-      .then((event) => {
-        this.setPort(event.ports[0]);
-        resolve();
-      });
-    });
-  }
-
-  postMessage() {
-    throw new Error('Missing a port. It has not connected yet.');
-  }
-
-  _setPort(next, current) {
-    if (current) {
-      current.onmessage = null;
-    }
-    next.onmessage = (event) => {
-      this.dispatchEvent(event);
-      if (event.data.method) {
-        const partialEvent = new Event(event.data.method + '.message');
-        partialEvent.data = event.data;
-        this.dispatchEvent(partialEvent);
-      }
-    };
-    this.postMessage = (...args) => {
-      next.postMessage(...args);
-    };
-    return next;
-  }
 }
 
 module.exports = Player;
